5539b136067e5f8f136522176f2e7e28f61ded4e
ben <ben.lenser@immobilienscout24.de>
2015-11-07 20:16:06 +0100

    change auth cookie to be secure

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -2,8 +2,8 @@ module.exports = {
      cookieOptions : {
         ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
         encoding: 'none',    // we already used JWT to encode
-        //isSecure: true,      // warm & fuzzy feelings
-        // isHttpOnly: true,    // prevent client alteration
+        isSecure: true,      // warm & fuzzy feelings
+        isHttpOnly: true,    // prevent client alteration
         clearInvalid: false, // remove invalid cookies
         strictHeader: true   // don't allow violations of RFC 6265
     },

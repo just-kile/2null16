1b662453c99314c33ffbc87e03cfb8d761ad1b5b
ben <ben.lenser@immobilienscout24.de>
2015-11-08 13:07:56 +0100

    enable secure cookie on live system

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,11 +1,26 @@
-module.exports = {
-     cookieOptions : {
-        ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
-        encoding: 'none',    // we already used JWT to encode
-        isSecure: true,      // warm & fuzzy feelings
-        isHttpOnly: true,    // prevent client alteration
-        clearInvalid: false, // remove invalid cookies
-        strictHeader: true   // don't allow violations of RFC 6265
-    },
-    jwtSecret: process.env.JWT_SECRET || "mySecretKey"
-};
+
+var argv = require('minimist')(process.argv.slice(2)),
+    isProd = argv.prod || false;
+if(isProd){
+    module.exports = {
+        cookieOptions : {
+            ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
+            encoding: 'none',    // we already used JWT to encode
+            isSecure: true,      // warm & fuzzy feelings
+            isHttpOnly: true,    // prevent client alteration
+            clearInvalid: false, // remove invalid cookies
+            strictHeader: true   // don't allow violations of RFC 6265
+        },
+        jwtSecret: process.env.JWT_SECRET || "mySecretKey"
+    };
+}else{
+    module.exports = {
+        cookieOptions : {
+            ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
+            encoding: 'none',    // we already used JWT to encode
+            clearInvalid: false, // remove invalid cookies
+            strictHeader: true   // don't allow violations of RFC 6265
+        },
+        jwtSecret: process.env.JWT_SECRET || "mySecretKey"
+    };
+}
